9d5a9f67a3ae2b36d8b95410ddae551302658069
Vitor Lucena <104996449+vitorlucena1@users.noreply.github.com>
2025-05-03 18:16:38 +0000

    Alteração de rota

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,5 +5,5 @@ const router = express.Router();
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-router.get('/', userController.getAllUsers);
+router.get('/users', userController.getAllUsers);
 export default router;
